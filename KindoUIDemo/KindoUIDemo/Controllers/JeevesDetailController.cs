9dc1141c34212c2c6545ba7296ecece74aea08f1
Praveen Agrawal <praveen.agrawal@jeeves.se>
2018-06-12 13:58:59 +0530

    TaghealperView with partial page.

---
+++ KindoUIDemo/KindoUIDemo/Controllers/JeevesDetailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KindoUIDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KindoUIDemo.Controllers
+{
+    [Route("JeevesDetail/Index/{id=1}")]
+    public class JeevesDetailController : Controller
+    {
+        private IListData _productData;
+
+        public JeevesDetailController(IListData productData)
+        {
+            _productData = productData;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var product = _productData.GetDetailData(id);
+            return View(product);
+        }
+    }
+}
--- KindoUIDemo/KindoUIDemo/Pages/MyTGDetail.cs
+++ KindoUIDemo/KindoUIDemo/Pages/MyTGDetail.cs
@@ -15,25 +15,10 @@ namespace KindoUIDemo.Pages
     {
         private IListData _productData;
         public Dictionary<String, String> product;
-        private MDFormLayout mdFormLayout;
-        public List<MDField> _fieldList = new List<MDField>();
+
         public MyTGDetail(IListData productData)
         {
             _productData = productData;
-            mdFormLayout = new MDFormLayout();
-            XElement xmlElement = XElement.Load(@"jiniDevExpress.xml");
-            var columnList = xmlElement.Elements().ElementAt(1).Elements().ElementAt(0).Elements();
-            mdFormLayout.FormColumnCount = columnList.Count();
-            for (int i = 0; i < mdFormLayout.FormColumnCount; i++)
-            {
-                var column = columnList.ElementAt(i);
-                foreach(var fieldElement in column.Elements())
-                {
-                    var feild = new MDField { ID = fieldElement.Attribute("Id").Value, ControlType = fieldElement.Attribute("Type").Value, Name = fieldElement.Attribute("PromptText").Value };
-                    mdFormLayout.FieldList.Add(feild);
-                    _fieldList.Add(feild);
-                }
-            }
         }
 
         public void OnGet(int id)
